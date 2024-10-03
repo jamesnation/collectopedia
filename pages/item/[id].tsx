27362e2651eada87afcf19f67f6a37804c45902b
jamesnation <jamesnation@gmail.com>
2024-10-03 12:48:32 +0100

    excess page

--- pages/item/[id].tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-import ItemDetailsPage from '@/components/item-details-page'
-
-export default function ItemPage() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return <ItemDetailsPage id={id as string} />
-}
